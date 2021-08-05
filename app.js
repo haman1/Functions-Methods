e716dda94e2f7a6a7a3d080d2fcf4bf2da32137e
ombasa <ombasaherman@gmail.com>
2021-08-05 13:42:10 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+//function declaration
+function greet(){
+    console.log('hello there');
+}
+
+//function expresion
+const speak = function(place){
+    console.log('where are you from');
+
+};
+greet();
+speak();
